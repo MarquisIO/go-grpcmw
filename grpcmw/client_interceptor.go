5c3862f5517fda2ed42cacac5650a8619e1a72c3
Maxime Fischer <maxime2fischer@gmail.com>
2017-04-12 16:55:22 +0100

    Add thread safety of client interceptors and registers.

--- grpcmw/client_interceptor.go
+++ grpcmw/client_interceptor.go
@@ -1,6 +1,8 @@
 package grpcmw
 
 import (
+	"sync"
+
 	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
@@ -25,18 +27,22 @@ type UnaryClientInterceptor interface {
 }
 
 type streamClientInterceptor struct {
+	lock         *sync.RWMutex
 	interceptors []grpc.StreamClientInterceptor
 }
 
 type unaryClientInterceptor struct {
+	lock         *sync.RWMutex
 	interceptors []grpc.UnaryClientInterceptor
 }
 
 // NewStreamClientInterceptor returns a new `StreamClientInterceptor`.
 // It initializes its interceptor chain with `arr`.
+// This implementation is thread-safe.
 func NewStreamClientInterceptor(arr ...grpc.StreamClientInterceptor) StreamClientInterceptor {
 	return &streamClientInterceptor{
 		interceptors: arr,
+		lock:         &sync.RWMutex{},
 	}
 }
 
@@ -55,15 +61,19 @@ func (si streamClientInterceptor) Interceptor() grpc.StreamClientInterceptor {
 	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
 		// TODO: Find a more efficient way
 		interceptor := streamer
+		si.lock.RLock()
 		for idx := len(si.interceptors) - 1; idx >= 0; idx-- {
 			interceptor = chainStreamClientInterceptor(si.interceptors[idx], interceptor)
 		}
+		si.lock.RUnlock()
 		return interceptor(ctx, desc, cc, method, opts...)
 	}
 }
 
 // AddGRPCInterceptor adds `arr` to the chain of interceptors.
 func (si *streamClientInterceptor) AddGRPCInterceptor(arr ...grpc.StreamClientInterceptor) StreamClientInterceptor {
+	si.lock.Lock()
+	defer si.lock.Unlock()
 	si.interceptors = append(si.interceptors, arr...)
 	return si
 }
@@ -72,6 +82,8 @@ func (si *streamClientInterceptor) AddGRPCInterceptor(arr ...grpc.StreamClientIn
 // to the chain of interceptors. It only calls the method `StreamInterceptor`
 // for each of them and append the return value to the chain.
 func (si *streamClientInterceptor) AddInterceptor(arr ...StreamClientInterceptor) StreamClientInterceptor {
+	si.lock.Lock()
+	defer si.lock.Unlock()
 	for _, i := range arr {
 		si.interceptors = append(si.interceptors, i.Interceptor())
 	}
@@ -80,9 +92,11 @@ func (si *streamClientInterceptor) AddInterceptor(arr ...StreamClientInterceptor
 
 // NewUnaryClientInterceptor returns a new `UnaryClientInterceptor`.
 // It initializes its interceptor chain with `arr`.
+// This implementation is thread-safe.
 func NewUnaryClientInterceptor(arr ...grpc.UnaryClientInterceptor) UnaryClientInterceptor {
 	return &unaryClientInterceptor{
 		interceptors: arr,
+		lock:         &sync.RWMutex{},
 	}
 }
 
@@ -101,15 +115,19 @@ func (ui *unaryClientInterceptor) Interceptor() grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		// TODO: Find a more efficient way
 		interceptor := invoker
+		ui.lock.RLock()
 		for idx := len(ui.interceptors) - 1; idx >= 0; idx-- {
 			interceptor = chainUnaryClientInterceptor(ui.interceptors[idx], interceptor)
 		}
+		ui.lock.RUnlock()
 		return interceptor(ctx, method, req, reply, cc, opts...)
 	}
 }
 
 // AddGRPCInterceptor adds `arr` to the chain of interceptors.
 func (ui *unaryClientInterceptor) AddGRPCInterceptor(arr ...grpc.UnaryClientInterceptor) UnaryClientInterceptor {
+	ui.lock.Lock()
+	defer ui.lock.Unlock()
 	ui.interceptors = append(ui.interceptors, arr...)
 	return ui
 }
@@ -118,6 +136,8 @@ func (ui *unaryClientInterceptor) AddGRPCInterceptor(arr ...grpc.UnaryClientInte
 // to the chain of interceptors. It only calls the method `UnaryInterceptor`
 // for each of them and append the return value to the chain.
 func (ui *unaryClientInterceptor) AddInterceptor(arr ...UnaryClientInterceptor) UnaryClientInterceptor {
+	ui.lock.Lock()
+	defer ui.lock.Unlock()
 	for _, i := range arr {
 		ui.interceptors = append(ui.interceptors, i.Interceptor())
 	}
