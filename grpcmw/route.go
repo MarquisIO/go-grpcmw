69fb728c50a8af149b6c5e511575e4176eff9d5e
Maxime Fischer <maxime2fischer@gmail.com>
2017-04-15 10:09:31 +0100

    Fix regexp according to grpc documentation.

--- grpcmw/route.go
+++ grpcmw/route.go
@@ -3,6 +3,5 @@ package grpcmw
 import "regexp"
 
 var (
-	// TODO: See https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#appendix-a---grpc-for-protobuf
-	routeRegexp = regexp.MustCompile("\\/(?:(.+)\\.(?:(.+)\\/(.+)|(.+))|(.+))")
+	routeRegexp = regexp.MustCompile(`\/(?:(.+)\.)?(.+)\/(.+)`)
 )
