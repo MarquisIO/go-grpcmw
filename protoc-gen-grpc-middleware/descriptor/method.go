bc3bdb99c082591a2e899762b4975e08217b0da2
Maxime Fischer <maxime2fischer@gmail.com>
2017-12-23 12:05:04 +0100

    Use stream interceptors on both sides with unidirectionnal streams.

--- protoc-gen-grpc-middleware/descriptor/method.go
+++ protoc-gen-grpc-middleware/descriptor/method.go
@@ -11,19 +11,17 @@ type Method struct {
 	Package      string
 	Service      string
 	Method       string
-	ClientStream bool
-	ServerStream bool
+	Stream       bool
 	Interceptors *Interceptors
 }
 
 // GetMethod parses `pb` and builds from it a `Method` object.
 func GetMethod(pb *descriptor.MethodDescriptorProto, service, pkg string) (method *Method, err error) {
 	method = &Method{
-		Package:      pkg,
-		Service:      service,
-		Method:       pb.GetName(),
-		ClientStream: pb.GetClientStreaming(),
-		ServerStream: pb.GetServerStreaming(),
+		Package: pkg,
+		Service: service,
+		Method:  pb.GetName(),
+		Stream:  pb.GetClientStreaming() || pb.GetServerStreaming(),
 	}
 	if pb.Options != nil {
 		if method.Interceptors, err = GetInterceptors(pb.Options, annotations.E_MethodInterceptors); err != nil {
