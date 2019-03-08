1a74725c5ab5a2e703301973b010907c31bda9de
Maxime Fischer <maxime2fischer@gmail.com>
2019-03-08 11:01:34 +0000

    Do not generate files that are not given in FileToGenerate field.

--- protoc-gen-grpc-middleware/descriptor/parse.go
+++ protoc-gen-grpc-middleware/descriptor/parse.go
@@ -8,12 +8,18 @@ import (
 // files information (value).
 func Parse(pb *plugin.CodeGeneratorRequest) (pkgs map[string][]*File, err error) {
 	// TODO: Do this in multiple goroutines
+	filesToGenerate := make(map[string]struct{}, len(pb.GetFileToGenerate()))
+	for _, f := range pb.GetFileToGenerate() {
+		filesToGenerate[f] = struct{}{}
+	}
 	pkgs = make(map[string][]*File)
 	for _, file := range pb.GetProtoFile() {
-		if parsed, err := GetFile(file); err != nil {
-			return nil, err
-		} else if parsed != nil {
-			pkgs[file.GetPackage()] = append(pkgs[file.GetPackage()], parsed)
+		if _, ok := filesToGenerate[file.GetName()]; ok {
+			if parsed, err := GetFile(file); err != nil {
+				return nil, err
+			} else if parsed != nil {
+				pkgs[file.GetPackage()] = append(pkgs[file.GetPackage()], parsed)
+			}
 		}
 	}
 	return
